6401020b86f13f9b6f91116e2a4d4c7acf05d6cc
Andrei Muntean <andreimuntean0@yahoo.com>
2019-10-22 22:25:21 +0300

    lab1,2,3 si temele de la lab2,3

---
+++ Students/Muntean Andrei/Laboratoare/lab2/src/lab2/p3.java
@@ -0,0 +1,39 @@
+package lab2;
+
+public class p3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MAX_VALUE+453535667);
+		System.out.println(Integer.MAX_VALUE-876);
+		
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Short.MAX_VALUE+234);
+		System.out.println(Short.MAX_VALUE-300);
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MAX_VALUE+10);
+		System.out.println(Long.MAX_VALUE-21);
+		
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MAX_VALUE+11);
+		System.out.println(Byte.MAX_VALUE-123);
+		
+		/* Nu am  inteles cele doua cerinte pentru float si double
+		 -reprezentati in virgula mobila numere cu un numar de cifre zecimale mai mare decat numarul
+		  de cifre reprezentabile exact
+		 -adunati sau scadeti cantitati din numere care au mai multe 
+		  cifre zecimale in reprezentarea in baza 10 decat permite reprezentarea în virgula mobila 
+		  
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.MAX_VALUE+123.56213);
+		System.out.println(Float.MAX_VALUE-980.23434);
+
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.MAX_VALUE+123.56213);
+		System.out.println(Double.MAX_VALUE-980.23434);
+		*/
+	}
+
+}
